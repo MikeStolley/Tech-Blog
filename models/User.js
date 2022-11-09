e5e415aab3d4a8ad6b0090a4a9809eefef9aed6b
Mike Stolley <stolleywilliam@yahoo.com>
2022-11-09 16:08:20 -0500

    updated User models and index.js 'hasMany'

---
+++ models/User.js
@@ -0,0 +1,36 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/config');
+
+class User extends Model {
+    checkPassword(login) {
+        return bcrypt.compareSync(login, this.password);
+    }
+}
+
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user',
+    }
+);
+
+module.exports = User;
