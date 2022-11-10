76976565e89432fd03e61804b007329fd6017165
Mike Stolley <stolleywilliam@yahoo.com>
2022-11-10 18:50:52 -0500

    get route for single post created

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -14,6 +14,7 @@ router.get('/', useAuth, async (req,res) => {
     }
 });
 
+
 router.get('/new', useAuth, (req, res) => {
     res.render('new-post', {
         layout: 'dashboard',
---
+++ controllers/home-routes.js
@@ -0,0 +1,15 @@
+const router = require('express').Router();
+const { Post, Comment, User } = require('../models/')
+const useAuth - require('../utils/auth');
+
+router.get('/', async (req, res) => {
+    try {
+        const postData = await Post.findAll({
+            include: [User],
+        })
+        const posting = postData.map((post) => postData.get({ plain: true }))
+        res.render('all-posts', { posts, loggedIn: req.session.loggedIn})
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
