559cafb5f6b32e1c4064e4554fac391c815fa88e
Mike Stolley <stolleywilliam@yahoo.com>
2022-11-10 18:34:57 -0500

    updated dashboard routes

---
+++ controllers/dashboard-routes.js
@@ -0,0 +1,24 @@
+const router = require('express').Router();
+const { Post, User } = require('../models/');
+const useAuth = require('../utils/auth');
+
+router.get('/', useAuth, async (req,res) => {
+    try{
+        const postData = await Post.findAll({
+            where:{'userId': req.session.userId},
+            include:[User]
+        });
+        const posting = postData.map((post) => post.get({ plain: true }));
+    } catch (err) {
+        res.redirect('login')
+    }
+});
+
+router.get('/new', useAuth, (req, res) => {
+    res.render('new-post', {
+        layout: 'dashboard',
+    });
+});
+
+
+module.exports = router;
