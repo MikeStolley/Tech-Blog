c8f0b8be4b629896b4c9d905e1248a3bb4a3951b
Mike Stolley <stolleywilliam@yahoo.com>
2022-11-10 19:02:55 -0500

    updated home-routes, finished what I could

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const { Post, Comment, User } = require('../models/')
-const useAuth - require('../utils/auth');
+const useAuth = require('../utils/auth');
 
+
+// GET all posts
 router.get('/', async (req, res) => {
     try {
         const postData = await Post.findAll({
@@ -13,4 +15,50 @@ router.get('/', async (req, res) => {
         res.status(500).json(err);
     }
 });
+
+// GET a single post
+router.get('/post/:id', useAuth, async(req, res) => {
+    try {
+        const postData = await Post.findOne({
+            where: {id: req.params.id},
+            include: [
+                User,
+                {
+                    model: Comment,
+                    include: [User],
+                },
+            ],
+        });
+        if (postData) {
+            const posts = postData.get({ plain: true });
+            res.render('single-post', { post, loggedIn: req.session.loggedIn })
+        } else {
+            res.status(404).end();
+        }
+    } catch (err) {
+        res.status(500).json(err)
+    }
+});
+
+// GET the login and signup routes
+
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/dashboard');
+        return;
+    }
+    res.render('login');
+});
+
+router.get('/signup', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/dashboard');
+        return;
+    }
+
+    res.render('signup')
+});
+
+module.exports = router;
+
 
